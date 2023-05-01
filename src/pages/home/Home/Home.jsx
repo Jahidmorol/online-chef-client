2498df2052765d2b3da554e91ab9cc8631e1e636
jahid morol <jahidmorol2@gmail.com>
2023-05-02 01:43:18 +0600

    added footer demo

--- src/pages/home/Home/Home.jsx
+++ src/pages/home/Home/Home.jsx
@@ -3,10 +3,10 @@ import shef from '../../../assets/shef.png'
 
 const Home = () => {
     return (
-        <div className='bg-yellow-600 md:flex flex-col items-center justify-between h-screen'>
-            <img src={shef} style={{height: '40rem'}} alt="" />
+        <div className='bg-yellow-600 md:flex items-center justify-between h-screen'>
+            <img src={shef} className="w-100 md:w-6/12" alt="" />
             <div>
-                <h2 className="text-3xl"></h2>
+                <h2 className="text-6xl"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis, modi!</h2>
             </div>
         </div>
     );
